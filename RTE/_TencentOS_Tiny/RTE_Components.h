94bfcee69dae9cc2e9821d2a77a618b7fd836e34
bxgj <bwysxl@126.com>
2020-10-28 23:56:52 +0800

    新建工程

---
+++ RTE/_TencentOS_Tiny/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TencentOS_tiny_EVB_G0' 
+ * Target:  'TencentOS_Tiny' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
