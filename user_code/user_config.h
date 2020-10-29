ff80570d28b5f6a49bcd851525c3864464b3287b
bxgj <bwysxl@126.com>
2020-10-30 00:15:53 +0800

    新增用户配置文件

---
+++ user_code/user_config.h
@@ -0,0 +1,11 @@
+#ifndef __USER_CONFIG_H
+#define __USER_CONFIG_H
+
+// 这里填入你自己的信息
+#define USER_PRODUCT_ID    "10字节产品ID"
+#define USER_DEVICE_NAME   "不超过48字节的设备名"
+#define USER_DEVICE_SECRET "24字节的设备密钥"
+#define USER_WIFI_SSID     "你的WIFI名"
+#define USER_WIFI_PW       "你的WIFI密码"
+
+#endif
